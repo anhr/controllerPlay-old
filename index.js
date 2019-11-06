6d1246493646f32fcaf7bfc23f6f22ecb9b07e19
Andrej <anhr@mail.ru>
2019-11-06 12:11:49 +0700

    import lang

--- index.js
+++ index.js
@@ -17,8 +17,8 @@
 //import { GUI, controllers } from '../../dat.gui';
 import { GUI, controllers } from '../../dat.gui/CustomController/build/dat.gui.module.js';
 
-import { getLanguageCode } from '../../commonNodeJS/master/build/common.module.js';
-//import { getLanguageCode } from '../../commonNodeJS/master/lang.js';
+//import { getLanguageCode } from '../../commonNodeJS/master/build/common.module.js';
+import { getLanguageCode } from '../../commonNodeJS/master/lang.js';
 //import { getLanguageCode } from 'http://localhost/anhr/commonNodeJS/master/lang.js';
 
 //Localization
