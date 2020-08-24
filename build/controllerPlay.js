e697aa3eb68ee205abeb1d965581cd9ca254b0a2
Andrej <anhr@mail.ru>
2020-08-24 08:59:36 +0700

    build

--- build/controllerPlay.js
+++ build/controllerPlay.js
@@ -2565,40 +2565,41 @@ var controllers = {
  * http://www.apache.org/licenses/LICENSE-2.0
  */
 function getLanguageCode() {
-  function _getLocale() {
-    if (!navigator) {
-      console.error("getLocale() failed! !navigator");
-      return "";
-    }
-    if (navigator.languages !== undefined && typeof navigator.languages !== 'unknown' && navigator.languages.length > 0) return navigator.languages[0];
-    if (navigator.language) {
-      return navigator.language;
-    } else if (navigator.browserLanguage) {
-      return navigator.browserLanguage;
-    } else if (navigator.systemLanguage) {
-      return navigator.systemLanguage;
-    } else if (navigator.userLanguage) {
-      return navigator.userLanguage;
-    }
-    console.error("getLocale() failed!");
-    return "";
-  }
-  return _getLocale().toLowerCase().match(/([a-z]+)(?:-([a-z]+))?/)[1];
+		function _getLocale() {
+				if (!navigator) {
+						console.error("getLocale() failed! !navigator");
+						return "";
+				}
+				if (navigator.languages !== undefined && typeof navigator.languages !== 'unknown'
+				&& navigator.languages.length > 0) return navigator.languages[0];
+				if (navigator.language) {
+						return navigator.language;
+				} else if (navigator.browserLanguage) {
+						return navigator.browserLanguage;
+				} else if (navigator.systemLanguage) {
+						return navigator.systemLanguage;
+				} else if (navigator.userLanguage) {
+						return navigator.userLanguage;
+				}
+				console.error("getLocale() failed!");
+				return "";
+		}
+		return _getLocale().toLowerCase().match(/([a-z]+)(?:-([a-z]+))?/)[1];
 }
 
 /**
-* PlayController class for using in my version of dat.gui(https://github.com/anhr/dat.gui) for playing of 3D objects in my projects.
-*
-* @author Andrej Hristoliubov https://anhr.github.io/AboutMe/
-*
-* @copyright 2011 Data Arts Team, Google Creative Lab
-*
-* @license under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*/
+ * PlayController class for using in my version of dat.gui(https://github.com/anhr/dat.gui) for playing of 3D objects in my projects.
+ *
+ * @author Andrej Hristoliubov https://anhr.github.io/AboutMe/
+ *
+ * @copyright 2011 Data Arts Team, Google Creative Lab
+ *
+ * @license under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
 var lang = {
 				prevSymbol: '←',
 				prevSymbolTitle: 'Go to previous animation scene',
@@ -2699,6 +2700,7 @@ var PlayController = function (_controllers$CustomCo) {
 																return buttons;
 												}
 								}, 'playRate', 1, 25, 1));
+								player.PlayController = _this2;
 								_this2.onRenamePlayButtons = function (playing) {
 												var name, title;
 												if (playing) {
@@ -2711,7 +2713,7 @@ var PlayController = function (_controllers$CustomCo) {
 												_renamePlayButtons(name, title, true);
 								};
 								_this2.onChangeRepeat = function () {
-												_renameRepeatButtons(player.getOptions().repeat);
+												_renameRepeatButtons(player.getSettings().repeat);
 								};
 								player.pushController(_this2);
 								_this2.onChange = function (value) {
@@ -2722,6 +2724,9 @@ var PlayController = function (_controllers$CustomCo) {
 								};
 								_this2.selectScene = function (index) {
 												_selectScene(parseInt(index));
+								};
+								_this2.setValue = function (value) {
+												this._controller.setValue(value);
 								};
 								return _this2;
 				}
